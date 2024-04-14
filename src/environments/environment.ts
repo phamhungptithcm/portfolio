65cecd0063978096a9fa4908b98ba2a1e1194c69
Hùng Phạm <phamhung.working@gmail.com>
2024-04-13 21:49:05 -0500

    init

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCzzpS0Kiy2a6_WPg2lHeUGiTjN2TmbFDQ",
+    authDomain: "hunpeo97.firebaseapp.com",
+    projectId: "hunpeo97",
+    storageBucket: "hunpeo97.appspot.com",
+    messagingSenderId: "154236648246",
+    appId: "1:154236648246:web:1e5e9f61b3c0698c609197",
+    measurementId: "G-9WRYYCQJVH",
+  },
+};
